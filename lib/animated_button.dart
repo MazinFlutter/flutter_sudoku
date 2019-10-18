5a02784d91557a29662623e1a7f5f93db389bb73
Mazin.SD <mymeemy@gmail.com>
2019-10-18 21:42:19 +0300

    Adding Multilingual comments.

--- lib/animated_button.dart
+++ lib/animated_button.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
+
+///A general animated button which must be provided with a name and a corresponding function.
+
 class AnimatedButton extends StatefulWidget {
 
+  //The text that'll be displayed in the middle of the button.
+  //النص الذي سيكون موجودا في منتصف الزر.
   @required
   final String title ;
 
+  //The function that'll be called when pressing this [AnimatedButton].
+  //الدالة التي سيتم استدعاؤها عند الضغط على الزر.
   @required
   final Function onPressed ;
 
@@ -15,8 +22,6 @@ class AnimatedButton extends StatefulWidget {
 }
 
 class _AnimatedButtonState extends State<AnimatedButton> with SingleTickerProviderStateMixin{
-
-  List<Color> colors = <Color>[Colors.green, Colors.orange, Colors.red] ;
 
   double _scale;
 
@@ -48,6 +53,8 @@ class _AnimatedButtonState extends State<AnimatedButton> with SingleTickerProvid
             width: MediaQuery.of(context).size.width/1.9,
             height: MediaQuery.of(context).size.height/12,
             child: RaisedButton(
+              //onPressed isn't provided because it absorb the press action from GestureDetector, and won't let the scaling animation to work properly, as onTapUp isn't triggered
+              //لم توضع قيمة للدالة onPressed وذلك لكونها تتجاوب مع ضغطة المستخدم وتمنع GestureDetector من عرض تأثير تغيير الحجم بصورة صحيحة.
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(MediaQuery.of(context).size.height/24),
                   side: BorderSide(color: Colors.lightBlueAccent)
@@ -61,6 +68,8 @@ class _AnimatedButtonState extends State<AnimatedButton> with SingleTickerProvid
 
   void _onTapDown(TapDownDetails details) async{
     _controller.forward();
+    //This delay is put so that the animation has some time to be shown to user before navigating.
+    //هذا التأخير موضوع بغرض اتاحة زمن لعرض تأثيير تغيير الحجم قبل الذهاب إلى صفحة أخرى.
     Future.delayed( Duration(seconds: 1), (){
       widget.onPressed();
     });
