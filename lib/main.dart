d18075e02ba8e30ae39cf506154e9361009cee4a
Mazin.SD <mymeemy@gmail.com>
2019-10-18 22:18:35 +0300

    Moved MyHomePage class into home_page.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'animated_button.dart';
-import 'app_localizations.dart';
 import 'app_localizations_delegate.dart';
 import 'bloc_base.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'highscores_page.dart';
-import 'instructions_page.dart';
-import 'numbers_board.dart';
-import 'settings_page.dart';
 import 'user_data_bloc.dart';
 
 void main() => runApp(
@@ -91,80 +85,5 @@ class MyApp extends StatelessWidget {
         }
       },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>{
-
-  double easyWidth ;
-  double mediumWidth ;
-  double hardWidth ;
-
-  @override
-  Widget build(BuildContext context) {
-    if( (easyWidth == null) | (mediumWidth == null) | (hardWidth == null)){
-      easyWidth = mediumWidth = hardWidth =  MediaQuery.of(context).size.width/1.5 ;
-    }
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage('assets/images/numbers_3.jpg'), fit: BoxFit.cover),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: Text(AppLocalizations.of(context, 'Title'),style: TextStyle(fontSize: 86.0, color: Colors.white, fontWeight: FontWeight.bold),),
-                ),
-              ],
-            ),
-            AnimatedButton(title: AppLocalizations.of(context, 'Continue'), onPressed: (){
-
-            }),
-            AnimatedButton(title: AppLocalizations.of(context, 'NewGame'), onPressed: (){
-              moveToGame(3);
-            }),
-            AnimatedButton(title: AppLocalizations.of(context, 'HighScores'), onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => HighScoresPage()));
-            },),
-            AnimatedButton(title: AppLocalizations.of(context, 'Settings'), onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage()));
-            },),
-            AnimatedButton(title: AppLocalizations.of(context, 'Instructions'), onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => InstructionsPage()));
-            },),
-            AnimatedButton(title: AppLocalizations.of(context, 'Exit'), onPressed: (){
-              exitApp();
-            }),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  exitApp(){
-    print('Good bye');
-    SystemNavigator.pop();
-  }
-
-  moveToGame(int selectedLevel){
-    Navigator.push(context, MaterialPageRoute(builder: (context) => NumbersBoard()));
   }
 }
