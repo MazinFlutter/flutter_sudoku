0bb83fb7bf3ced7d472aba775339de1a5e952bf8
Mazin.SD <mymeemy@gmail.com>
2019-10-19 15:29:55 +0300

    Wrapped buttons with StreamBuilder to consume its data in more than one.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -32,46 +32,67 @@ class _MyHomePageState extends State<MyHomePage>{
         decoration: BoxDecoration(
           image: DecorationImage(image: AssetImage('assets/images/numbers_3.jpg'), fit: BoxFit.cover),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: StreamBuilder<bool>(
+          stream: userBloc.getIsThereAPreviousGame(),
+          builder: (context, isThereAGameSnapshot){
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Text(AppLocalizations.of(context, 'Title'),style: TextStyle(fontSize: 86.0, color: Colors.white, fontWeight: FontWeight.bold),),
+                    ),
+                  ],
+                ),
+                isThereAGameSnapshot.data ? AnimatedButton(title: AppLocalizations.of(context, 'Continue'), onPressed: (){
+
+                }) : Container(),
+                AnimatedButton(title: AppLocalizations.of(context, 'NewGame'), onPressed: (){
+                  if(isThereAGameSnapshot.data){
+                    showDialog(context: context, builder: (BuildContext context){
+                      return CupertinoAlertDialog(
+                        title: Text(AppLocalizations.of(context, 'UnfinishedGame'), style: TextStyle(fontSize: 22.0),),
+                        content: Text(AppLocalizations.of(context, 'UnfinishedGameMessage')),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text(AppLocalizations.of(context, 'Cancel')),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: Text(AppLocalizations.of(context, 'StartANewGame')),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              moveToGame(3);
+                            },
+                          ),
+                        ],
+                      );
+                    });
+                  }
+                }),
+                AnimatedButton(title: AppLocalizations.of(context, 'HighScores'), onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HighScoresPage()));
+                },),
+                AnimatedButton(title: AppLocalizations.of(context, 'Settings'), onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage()));
+                },),
+                AnimatedButton(title: AppLocalizations.of(context, 'Instructions'), onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => InstructionsPage()));
+                },),
+                AnimatedButton(title: AppLocalizations.of(context, 'Exit'), onPressed: (){
+                  exitApp();
+                }),
                 Padding(
                   padding: EdgeInsets.all(15.0),
-                  child: Text(AppLocalizations.of(context, 'Title'),style: TextStyle(fontSize: 86.0, color: Colors.white, fontWeight: FontWeight.bold),),
                 ),
               ],
-            ),
-            StreamBuilder<bool>(
-              stream: userBloc.getIsThereAPreviousGame(),
-              initialData: false,
-              builder: (context, isThereAGameSnapshot){
-                return isThereAGameSnapshot.data ? AnimatedButton(title: AppLocalizations.of(context, 'Continue'), onPressed: (){
-
-                }) : Container() ;
-              },
-            ),
-            AnimatedButton(title: AppLocalizations.of(context, 'NewGame'), onPressed: (){
-              moveToGame(3);
-            }),
-            AnimatedButton(title: AppLocalizations.of(context, 'HighScores'), onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => HighScoresPage()));
-            },),
-            AnimatedButton(title: AppLocalizations.of(context, 'Settings'), onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => SettingsPage()));
-            },),
-            AnimatedButton(title: AppLocalizations.of(context, 'Instructions'), onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => InstructionsPage()));
-            },),
-            AnimatedButton(title: AppLocalizations.of(context, 'Exit'), onPressed: (){
-              exitApp();
-            }),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
