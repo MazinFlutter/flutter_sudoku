e8bc6f3ead0ee55d0867db285507a75f4ead566d
Mazin.SD <mymeemy@gmail.com>
2019-10-23 15:17:35 +0300

    valued moved inside startANewGame() body.

--- lib/user_data_bloc.dart
+++ lib/user_data_bloc.dart
@@ -94,10 +94,6 @@ class UserDataBloc extends BlocBase{
 
     List<String> convertedEditableFieldsArray = List(81) ;
 
-    //final response = await Client().get('http://www.cs.utep.edu/cheon/ws/sudoku/new/?size=9&?level=3');
-
-    //BoardNumbers initialNumbers = parseNumbers(response.body);
-
     BoardNumbers initialNumbers = parseNumbers('{"response":true,"size":"9","squares":[{"x":0,"y":1,"value":4},{"x":0,"y":5,"value":7},{"x":0,"y":7,"value":8},{"x":1,"y":0,"value":2},{"x":1,"y":2,"value":7},{"x":1,"y":3,"value":4},{"x":1,"y":5,"value":9},{"x":1,"y":7,"value":5},{"x":2,"y":0,"value":3},{"x":2,"y":4,"value":8},{"x":2,"y":6,"value":4},{"x":2,"y":7,"value":7},{"x":3,"y":2,"value":3},{"x":3,"y":4,"value":5},{"x":3,"y":5,"value":2},{"x":3,"y":6,"value":8},{"x":4,"y":1,"value":6},{"x":4,"y":3,"value":7},{"x":4,"y":4,"value":1},{"x":4,"y":5,"value":3},{"x":4,"y":7,"value":4},{"x":5,"y":0,"value":5},{"x":5,"y":2,"value":2},{"x":5,"y":4,"value":6},{"x":5,"y":6,"value":7},{"x":5,"y":8,"value":9},{"x":6,"y":0,"value":6},{"x":6,"y":1,"value":5},{"x":6,"y":3,"value":2},{"x":6,"y":4,"value":7},{"x":6,"y":6,"value":3},{"x":7,"y":1,"value":3},{"x":7,"y":3,"value":5},{"x":7,"y":5,"value":6},{"x":7,"y":6,"value":1},{"x":8,"y":0,"value":7},{"x":8,"y":1,"value":2},{"x":8,"y":3,"value":3},{"x":8,"y":7,"value":6},{"x":8,"y":8,"value":8}]}');
 
     if(initialNumbers.response){
