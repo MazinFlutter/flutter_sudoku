e69200db04dd6b1cace9cb226ad87de17e7b9f74
Mazin.SD <mymeemy@gmail.com>
2019-10-30 18:48:39 +0300

    changed the high scores app bar's title to a localized one.

--- lib/highscores_page.dart
+++ lib/highscores_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sudoku/app_localizations.dart';
 
 class HighScoresPage extends StatefulWidget {
 
@@ -15,7 +16,7 @@ class _HighScoresPageState extends State<HighScoresPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('High Scores'),),
+      appBar: AppBar(title: Text(AppLocalizations.of(context, 'HighScores')),),
       body: Center(child: Text('HighScores Page')),
     );
   }
